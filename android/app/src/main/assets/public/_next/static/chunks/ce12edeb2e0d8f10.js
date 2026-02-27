94811029e3c4b891cd682fbc655938b07cef9f8e
James7774 <james7774@example.com>
2026-02-27 15:00:30 +0500

    Update: Synchronized Android assets and updated README

---
+++ android/app/src/main/assets/public/_next/static/chunks/ce12edeb2e0d8f10.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,62304,e=>{"use strict";var i=e.i(20364),t=e.i(44018);e.s(["FirebaseAuthentication",()=>i.FirebaseAuthentication,"Persistence",()=>t.Persistence,"ProviderId",()=>t.ProviderId])}]);
